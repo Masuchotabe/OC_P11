67994f872393474aefd38e314deca29e84f6fd8a
Thomas MOREAU <tmoreau@q1c1.fr>
2024-06-03 14:03:49 +0200

    Add performances tests

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -6,6 +6,29 @@ class WebsiteUser(HttpUser):
     def index_page(self):
         self.client.get('/')
 
+    @task
+    def points_display(self):
+        self.client.get('/pointsDisplay')
+
+    @task
     def login(self):
-        self.client.post('/showSummary', )
+        self.client.post('/showSummary', {'email': 'john@simplylift.co'})
+
+    @task
+    def booking_places(self):
+        self.client.get('/book/Summer Strongman/Simply Lift')
+
+    @task
+    def purchase_places(self):
+        self.client.post('/purchasePlaces', {'club': '', 'competition': 'Summer Strongman'})
+
+
+    # @task
+    # def ma_funct(self):
+    # @task
+    # def ma_funct(self):
+    # @task
+    # def ma_funct(self):
+
+
 
